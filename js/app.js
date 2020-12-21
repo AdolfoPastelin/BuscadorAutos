7184580fe3ebbcf8b273dd082fa875833ed1ad1d
AdolfoPastelin <adolfo.pe97@gmail.com>
2020-12-21 04:55:46 -0600

    Se añadió la funcionalidad de seleccion de year

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 // Variables //
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+//obtiene el año actual (2020) y el minimo año para ventas (2010)
+const max = new Date().getFullYear(); //al pasar de año, se cambiará automaticamente.
+const min = max - 10;
 
 // eventos //
 document.addEventListener('DOMContentLoaded', () => {
+	//Muestra los autos nadamas al cargar la página (para despues filtrar con otros metodos)
 	mostrarAutos();
+
+	//Llena las opciones de años
+	llenarSelect();
 });
 
 
-// funciones //
+// Funciones //
 //Esta función mostrará los autos que se tendrán en una DB
 //En este caso no tenemos una base de datos pero usaremos un objeto en JS
 function mostrarAutos() {
@@ -24,4 +33,16 @@ function mostrarAutos() {
 		//insertar en el html
 		resultado.appendChild(autoHTML);
 	});
+}
+
+//Generar los siguientes años del select
+function llenarSelect() {
+	for(let i = max; i >= min; i--){
+		console.log(i);
+
+		const opcion = document.createElement('option');
+		opcion.value = i;
+		opcion.textContent = i;
+		year.appendChild(opcion); //Agrega las opciones de año al select.
+	}
 }
