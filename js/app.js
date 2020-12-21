b67b8cf02d46d88bf4b1dbdab0c8a7c714adeae2
AdolfoPastelin <adolfo.pe97@gmail.com>
2020-12-20 22:27:39 -0600

    Estructura del proyecto, HTML, CSS y objeto auto

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Variables //
+
+
+// eventos //
+document.addEventListener('DOMContentLoaded', () => {
+	mostrarAutos();
+});
+
+
+// funciones //
+//Esta función mostrará los autos que se tendrán en una DB
+//En este caso no tenemos una base de datos pero usaremos un objeto en JS
+function mostrarAutos() {
+	console.log('mostrando auto...');
+}
