3886a6f7b9bf46d9f01596b12391b9eecb222875
AdolfoPastelin <adolfo.pe97@gmail.com>
2020-12-20 23:47:19 -0600

    función mostrarAutos creada

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Variables //
-
+const resultado = document.querySelector('#resultado');
 
 // eventos //
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,5 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
 //Esta función mostrará los autos que se tendrán en una DB
 //En este caso no tenemos una base de datos pero usaremos un objeto en JS
 function mostrarAutos() {
-	console.log('mostrando auto...');
+	autos.forEach( (auto) => {
+		
+		const {marca, modelo, year, precio, puertas, color, transmision} = auto;
+		const autoHTML = document.createElement('p');
+
+		autoHTML.textContent = `${marca} ${modelo} - ${year} - ${puertas} Puertas
+		- Trasmisión: ${transmision} - Precio: ${precio} - Color: ${color}
+		
+		`;
+
+		//insertar en el html
+		resultado.appendChild(autoHTML);
+	});
 }
