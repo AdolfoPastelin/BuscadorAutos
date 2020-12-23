be5634419a93fcf6e4ea3473751eda7eb8089611
AdolfoPastelin <adolfo.pe97@gmail.com>
2020-12-22 21:23:34 -0600

    se agregaron los filtros - puertas, transmision y color

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const datosBusqueda = { //objeto datosBusqueda
 // eventos //
 document.addEventListener('DOMContentLoaded', () => {
 	//Muestra los autos nadamas al cargar la página (para despues filtrar con otros metodos)
-	mostrarAutos();
+	mostrarAutos(autos);
 
 	//Llena las opciones de años
 	llenarSelect();
@@ -38,51 +38,68 @@ document.addEventListener('DOMContentLoaded', () => {
 marca.addEventListener('change', (e) => { //evento que se dispara cuando ocurre un cambio en el atributo
 	datosBusqueda.marca = e.target.value;
 
-	filtrarAuto();
+	filtrarAuto(); // se manda a llamar para que actue en tiempo real
 });
 
 year.addEventListener('change', (e) => {
 	datosBusqueda.year = parseInt(e.target.value);
+
+	filtrarAuto();
 });
 
 minimo.addEventListener('change', (e) => {
 	datosBusqueda.minimo = parseInt(e.target.value);
+
+	filtrarAuto();
 });
 
 maximo.addEventListener('change', (e) => {
 	datosBusqueda.maximo = parseInt(e.target.value);
+
+	filtrarAuto();
 });
 
 puertas.addEventListener('change', (e) => {
 	datosBusqueda.puertas = parseInt(e.target.value);
+
+	filtrarAuto();
 });
 
 transmision.addEventListener('change', (e) => {
 	datosBusqueda.transmision = e.target.value;
+
+	filtrarAuto();
 });
 
 color.addEventListener('change', (e) => {
 	datosBusqueda.color = e.target.value;
-	console.log(datosBusqueda);
+
+	filtrarAuto();
 });
 
 // Funciones //
 //Esta función mostrará los autos que se tendrán en una DB
 //En este caso no tenemos una base de datos pero usaremos un objeto en JS
-function mostrarAutos() {
+function mostrarAutos(autos) {
+	limpiarHTML(); //Elimina el HTML previo
+
 	autos.forEach((auto) => {
 
 		const { marca, modelo, year, precio, puertas, color, transmision } = auto;
 		const autoHTML = document.createElement('p');
 
 		autoHTML.textContent = `${marca} ${modelo} - ${year} - ${puertas} Puertas
-		- Trasmisión: ${transmision} - Precio: ${precio} - Color: ${color}
-		
-		`;
+		- Trasmisión: ${transmision} - Precio: ${precio} - Color: ${color}`;
 
 		//insertar en el html
 		resultado.appendChild(autoHTML);
 	});
+}
+
+function limpiarHTML() {
+	while (resultado.firstChild) {
+		resultado.removeChild(resultado.firstChild);
+	}
 }
 
 //Generar los siguientes años del select
@@ -99,9 +116,12 @@ function llenarSelect() {
 //	esta función se considera de alto nivel, ya que tiene 
 //	como parametro otra función
 function filtrarAuto() {
-	const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
+	const resultado = autos.filter(filtrarMarca).filter(filtrarYear)
+		.filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision)
+		.filter(filtrarColor);
 
-	console.log(resultado);
+	mostrarAutos(resultado);
+	/* console.log(resultado); */
 }
 
 //Función que devuelve el auto buscado por el usuario en base a la marca
@@ -111,7 +131,7 @@ function filtrarMarca(auto) {
 	if (marca) {
 		return auto.marca === marca;
 	}
-	
+
 	return auto;
 }
 
@@ -123,5 +143,55 @@ function filtrarYear(auto) {
 		return auto.year === year;
 	}
 
-	return auto
+	return auto;
+}
+
+function filtrarMinimo(auto) {
+	const { minimo } = datosBusqueda;
+
+	if (minimo) {
+		return auto.precio >= minimo;
+	}
+
+	return auto;
+}
+
+function filtrarMaximo(auto) {
+	const { maximo } = datosBusqueda;
+
+	if (maximo) {
+		return auto.precio <= maximo;
+	}
+
+	return auto;
+}
+
+function filtrarPuertas(auto) {
+	const { puertas } = datosBusqueda;
+
+	if (puertas) {
+		return auto.puertas === puertas;
+	}
+
+	return auto;
+}
+
+function filtrarTransmision(auto) {
+	const { transmision } = datosBusqueda;
+
+	if (transmision) {
+		return auto.transmision === transmision;
+	}
+
+	return auto;
+}
+
+function filtrarColor(auto) {
+	const { color } = datosBusqueda;
+
+	if (color) {
+		return auto.color === color;
+	}
+
+	return auto;
 }
